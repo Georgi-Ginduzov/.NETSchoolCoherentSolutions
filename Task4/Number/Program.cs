e0530acb05a44d5bb72da6d71bd7e8f76c5d05bb
Georgi <georgi.ginduzov17d@gmail.com>
2024-05-14 23:46:42 +0300

    Implement rational number

---
+++ Task4/Number/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Task4/Number/RationalNumber.cs
@@ -0,0 +1,93 @@
+﻿namespace Number
+{
+    public sealed class RationalNumber : IComparable
+    {
+        int _numerator;
+        int _denominator;
+
+        public RationalNumber(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new ArgumentException("Denominator cannot be zero.");
+            }
+
+            int gcd = GCD(numerator, denominator);
+            Numerator = numerator / gcd;
+            Denominator = denominator / gcd;
+
+        }
+
+        private int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public int Numerator { get => _numerator; private set => _numerator = value; }
+        public int Denominator { get => _denominator; private set => _denominator = value; }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is RationalNumber)
+            {
+                RationalNumber other = obj as RationalNumber;
+                return Numerator == other.Numerator && Denominator == other.Denominator;
+            }
+
+            return false;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is RationalNumber)
+            {
+                RationalNumber other = obj as RationalNumber;
+                return ((double)this.Numerator / this.Denominator).CompareTo((double)other.Numerator / other.Denominator);
+            }
+
+            return -1;
+        }
+
+
+        public static RationalNumber operator +(RationalNumber r1, RationalNumber r2)
+        {
+            return new RationalNumber(r1.Numerator * r2.Denominator + r2.Numerator * r1.Denominator, r1.Denominator * r2.Denominator);
+        }
+
+        public static RationalNumber operator -(RationalNumber r1, RationalNumber r2)
+        {
+            return new RationalNumber(r1.Numerator * r2.Denominator - r2.Numerator * r1.Denominator, r1.Denominator * r2.Denominator);
+        }
+
+        public static RationalNumber operator *(RationalNumber r1, RationalNumber r2)
+        {
+            return new RationalNumber(r1.Numerator * r2.Numerator, r1.Denominator * r2.Denominator);
+        }
+
+        public static RationalNumber operator /(RationalNumber r1, RationalNumber r2)
+        {
+            return new RationalNumber(r1.Numerator * r2.Denominator, r1.Denominator * r2.Numerator);
+        }
+
+        public static explicit operator double(RationalNumber r)
+        {
+            return (double)r.Numerator / r.Denominator;
+        }
+
+        public static implicit operator RationalNumber(int i)
+        {
+            return new RationalNumber(i, 1);
+        }
+    }
+}
