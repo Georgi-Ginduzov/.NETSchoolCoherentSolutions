caf5dc3cb4a4ff1a64d5d8d3bf9055cff6850f23
Georgi <georgi.ginduzov17d@gmail.com>
2024-05-19 15:16:31 +0300

    Auto stash before merge of "main" and "origin/main"

---
+++ Task3/Queue/Queue.cs
@@ -0,0 +1,36 @@
+﻿namespace Queue
+{
+    public class Queue<T> : IQueue<T> where T : struct
+    {
+        private LinkedList<T> _list = new LinkedList<T>();
+
+        public void Enqueue(T item)
+        {
+            if (_list == null)
+            {
+                throw new NullReferenceException("Queue is not initialized in order to enqueue items to it!");
+            }
+
+            _list.AddLast(item);
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty. You cannot Dequeue an empty queue!");
+            }
+
+
+            T value = _list.First.Value;
+            _list.RemoveFirst();
+
+            return value;
+        }
+
+        public bool IsEmpty()
+        {
+            return !_list.Any();
+        }
+    }
+}
