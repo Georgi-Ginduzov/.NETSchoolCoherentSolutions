26acffc37c29616e1717c8ad2e89fbde38352f18
Georgi <georgi.ginduzov17d@gmail.com>
2024-05-29 18:33:13 +0300

    Refactorings

--- Task5/BookCollection/Program.cs
+++ Task5/BookCollection/Program.cs
@@ -1,4 +1,7 @@
 ﻿using BookCollection.Contracts;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace BookCollection
 {
@@ -8,33 +11,23 @@ namespace BookCollection
         {
             ICatalog catalog = new Catalog();
 
-            Book book1 = BookFactory.CreateBook("Book One", new DateTime(2001, 5, 20), new List<string> { "Author A", "Author B" });
-            Book book2 = BookFactory.CreateBook("Book Two", new DateTime(1999, 8, 15), new List<string> { "Author A" });
-            Book book3 = BookFactory.CreateBook("Book Three", new DateTime(2010, 3, 10), new List<string> { "Author C" });
+            var book1 = new Book("The alchemist", new DateTime(2001, 5, 20), new List<string> { "Paulo Coelho"});
+            var book2 = new Book("Atomic habits", new DateTime(1999, 8, 15), new List<string> { "James Clear" });
+            var book3 = new Book("Good Omens", new DateTime(2010, 3, 10), new List<string> { "Terry Pratchett", "Neil Gaiman" });
+            var book4 = new Book("Beautiful Creatures", new DateTime(2010, 3, 10), new List<string> { "Kami Garcia", "Margaretvar Stohl" });
+            var book5 = new Book("Nick & Norah's Infinite Playlist", new DateTime(2010, 3, 10), new List<string> { "Rachel Cohn", "David Levithan" });
 
             catalog.AddBook("123-4-56-789012-3", book1);
-            catalog.AddBook("1234567890123", book2);
+            catalog.AddBook("1234568890123", book2);
             catalog.AddBook("456-7-89-012345-6", book3);
+            catalog.AddBook("786-7-43-012345-6", book4);
+            catalog.AddBook("123-7-89-012345-6", book5);
 
-            ISortStrategy sortStrategy = new SortByTitleStrategy();
-            Console.WriteLine("Sorted book titles:");
-            foreach (var title in sortStrategy.Sort(catalog.GetSortedBookTitles().Select(t => catalog.GetBook(t))))
-            {
-                Console.WriteLine(title.Title);
-            }
-
-            Console.WriteLine("\nBooks by Author A:");
-            foreach (var book in catalog.GetBooksByAuthor("Author A"))
-            {
-                Console.WriteLine($"{book.Title} ({book.PublicationDate?.ToString("yyyy-MM-dd")})");
-            }
-
-            Console.WriteLine("\nAuthor book counts:");
-            foreach (var (author, count) in catalog.GetAuthorBookCounts())
-            {
-                Console.WriteLine($"{author} - {count} book(s)");
-            }
+            
+            
         }
+
+        
     }
     
 }
